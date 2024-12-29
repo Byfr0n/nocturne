5536913d657edba39783128c00695504191de8c7
Byfr0n <Byfr0n@users.noreply.github.com>
2024-12-29 18:00:01 +0200

    Initial push

---
+++ src/Loader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace nocturne
+{
+    public class Loader : MonoBehaviour
+    {
+        public static GameObject load;
+
+        public static void Init()
+        {
+            if (load == null)
+            {
+                load = new GameObject();
+
+                DontDestroyOnLoad(load);
+            }
+        }
+
+        public static void Unload()
+        {
+            if (load != null)
+            {
+                Destroy(load);
+            }
+        }
+    }
+}
